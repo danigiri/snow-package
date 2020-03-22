e5d162a59798b57539b8bc18aa454385ae021791
Daniel Giribet <dani@calidos.cat>
2020-03-23 00:29:18 +0100

    Reload on save to maintain source coherency

--- src/test/react/src/components/data2.jsx
+++ src/test/react/src/components/data2.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
-export function Data(props) {
+export function Data2(props) {
 	return 	<div class="card">
 				<div class="card-body">
 				<div class="card-title">Max two elements: {props.text}</div>
 				<p class="card-text">{props.number}</p>
 				</div>
 			</div>;
-
 }
--- src/test/react/src/components/data5.jsx
+++ src/test/react/src/components/data5.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import { Data3 } from './data3';
 
 export function Data5(props) {
-	
+
 	const firstPart = Data3(props);
+
 	return 	<>
 				<span className="badge badge-info">From POST</span>
 				{firstPart}
 			</>;
+
 }
