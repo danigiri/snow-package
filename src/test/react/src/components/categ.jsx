d958ae5dd686317be8ab331d4f0b0320c7ac3483
Daniel Giribet <dani@calidos.cat>
2020-03-30 23:54:50 +0200

    copyright in angular

--- src/test/react/src/components/categ.jsx
+++ src/test/react/src/components/categ.jsx
@@ -5,7 +5,7 @@ export function Categ(props) {
 	if (!props.value0x && !props.value0y && !props.value1x && !props.value1y) {
 		return <li className="list-group-item list-group-item-warning">Add some values</li>;
 	}
-	
+
 	const value0x = props.value0x ? <li className="list-group-item list-group-item-primary">{props.value0x}</li> : '';
 	const value1x = props.value1x ? <li className="list-group-item list-group-item-primary">{props.value1x}</li> : '';
 	const value0y = props.value0y ? <li className="list-group-item list-group-item-secondary">{props.value0y}</li> : '';
@@ -18,4 +18,21 @@ export function Categ(props) {
 				{value0y}
 				{value1y}
 			</ul>;
+
 }
+
+/*
+ *	  Copyright 2020 Daniel Giribet
+ *
+ *	 Licensed under the Apache License, Version 2.0 (the "License");
+ *	 you may not use this file except in compliance with the License.
+ *	 You may obtain a copy of the License at
+ *
+ *		 http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	 Unless required by applicable law or agreed to in writing, software
+ *	 distributed under the License is distributed on an "AS IS" BASIS,
+ *	 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	 See the License for the specific language governing permissions and
+ *	 limitations under the License.
+ */
